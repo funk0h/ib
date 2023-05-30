b3cd2d970d7cb3313cab285ed318da0afec84e82
funk0h <h.branez.s@gmail.com>
2023-05-30 09:16:09 -0400

    final

--- webdriver_p2p.py
+++ webdriver_p2p.py
@@ -32,7 +32,7 @@ def f_cambiartipo(tipo):
 def f_nextpage():
     nextpage_button = driver.find_element(By.XPATH, "//button[@id='next-page']")
     nextpage_button.click()
-def f_obtenerdatos(tipo_i,fiat_i,crypto_i):
+def f_obtenerdatos(tipo_i,fiat_i,crypto_i,datos_total):
     
     xpath_anunciante = "//div[@class='css-1jb7fpj']"
     xpath_ordenes = "//div[@class='css-1a0u4z7']"
@@ -50,11 +50,12 @@ def f_obtenerdatos(tipo_i,fiat_i,crypto_i):
                   xpath_limites,
                   xpath_pago]
     
-    datos = driver.find_elements(By.XPATH, xpath_anunciante)
-    i_total = len(datos)
-
+    datos_total_driven = len(driver.find_elements(By.XPATH, xpath_anunciante))
+    if datos_total_driven < datos_total:
+        datos_total = datos_total_driven
+        
     list_i = []
-    for i in range(0,i_total):
+    for i in range(0,datos_total):
         list_datos = [tipo_i,fiat_i,crypto_i]
         for xpath_i in list_xpath:
             driver_xpath = driver.find_elements(By.XPATH, xpath_i)
@@ -99,29 +100,29 @@ wb = xw.Book(path+file)
 sheet1 = wb.sheets['Sheet1']
 sheet2 = wb.sheets['Hoja1']
 fila_inicio = 2 #fila en la que inicia a copiar en excel
-col_inicio = 'A'
+col_inicio = 'A' #columna en la que inicia a copiar en excel
 
-max_datos_hoja = 10 #numero de filas de datos mostrados en binance, por hoja
+max_datos_hoja = int(sheet1.range('B1').value) #numero de filas de datos mostrados en binance, por hoja
+datos_total = int(sheet1.range('B2').value) #numero de filas (anunciantes) a guardar
 
-max_iteracion_hoja_tfc = 2 #tfcm: combinación de tipo-fiat-crypto-metodo de pago
+range_tipo = sheet1.range('B5:B14')
+list_tipo = range_tipo.options(ndim=1).value
+list_tipo = list(filter(None, list_tipo))
 
-list_tipo = ['compra','venta'] 
-#list_tipo = ['compra'] #temporal hasta desarrollar la def f_cambiartipo
-
-range_fiat = sheet1.range('B2:B10')
+range_fiat = sheet1.range('C5:C14')
 list_fiat = range_fiat.options(ndim=1).value
 list_fiat = list(filter(None, list_fiat))
 
-range_mpago = sheet1.range('C2:C10')
+range_mpago = sheet1.range('D5:D14')
 list_mpago = range_mpago.options(ndim=1).value
 list_mpago = list(filter(None,list_mpago))
 
-range_crypto = sheet1.range('D2:D10')
+range_crypto = sheet1.range('E5:E14')
 list_crypto = range_crypto.options(ndim=1).value
 list_crypto = list(filter(None,list_crypto))
-list_crypto = ['USDT'] #temporal hasta desarrollar la def f_cambiarcrypto
+#list_crypto = ['USDT'] #temporal 
 
-timesleep = 6
+timesleep = 5
 
 #endregion
 
@@ -133,6 +134,7 @@ driver.get(url)
 driver.maximize_window()
 #endregion
 
+sheet2.range('A1').value = ['Tipo','Fiat','Crypto','Anunciante','Ordenes','%Completado','Precio','Disponible','Límite Inf','Límite Sup','Pago1','Pago2','Pago3','Pago4','Pago5','Pago6','Pago7','Pago8','Pago9','Pago10']
 for tipo_i in list_tipo:
     f_cambiartipo(tipo_i)
     time.sleep(timesleep)
@@ -148,20 +150,21 @@ for tipo_i in list_tipo:
             for crypto_i in list_crypto:
                 #f_cambiarcrypto(crypto_i)
                 
-                iteracion_hoja = 0
+                datos_i = 0
                 repeticion = True
                 while repeticion == True:
-                    list_i = f_obtenerdatos(tipo_i,fiat_i,crypto_i) #hasta 10 filas de datos
+                    list_i = f_obtenerdatos(tipo_i,fiat_i,crypto_i,datos_total) #
                     sheet2.range(col_inicio+str(fila_inicio)).value = list_i
 
                     fila_inicio = fila_inicio + len(list_i)
 
-                    iteracion_hoja = iteracion_hoja + 1
+                    datos_i = datos_i + len(list_i)
 
-                    if len(list_i) < max_datos_hoja or iteracion_hoja == max_iteracion_hoja_tfc:
-                        repeticion = False
-                    else:
+                    if datos_i < datos_total:
                         f_nextpage()
                         #driver.implicitly_wait(10)
                         time.sleep(timesleep)
+                    else:
+                        repeticion = False                      
+                        
 driver.quit()
